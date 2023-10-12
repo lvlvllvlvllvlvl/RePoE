b473485cfce3ec1d9ef8ef69f538f90e7b77eb8d
ivy <119665497+lvlvllvlvllvlvl@users.noreply.github.com>
2023-10-12 21:07:19 +0700

    essence, synthesis and delve mods by base

--- RePoE/parser/modules/mods_by_base.py
+++ RePoE/parser/modules/mods_by_base.py
@@ -1,6 +1,8 @@
 import json
 from collections import OrderedDict
 
+import requests
+
 
 from RePoE.parser import Parser_Module
 from RePoE.parser.util import call_with_default_args, write_json
@@ -34,11 +36,12 @@ class mods_by_base(Parser_Module):
         with open(self.data_path + "item_classes.min.json") as f:
             item_classes: dict = json.load(f)
         with open(self.data_path + "mods.min.json") as f:
-            mods: dict[str, dict[str, dict]] = {}
-            for mod_id, mod in json.load(f).items():
+            mods = json.load(f)
+            mods_by_domain: dict[str, dict[str, dict]] = {}
+            for mod_id, mod in mods.items():
                 if mod["generation_type"] in ["blight_tower", "unique", "tempest", "enchantment", "crucible_tree"]:
                     continue
-                mods.setdefault(mod["domain"], {})[mod_id] = mod
+                mods_by_domain.setdefault(mod["domain"], {})[mod_id] = mod
 
         for base_id, base in base_items.items():
             item_class: dict = item_classes[base["item_class"]]
@@ -53,27 +56,63 @@ class mods_by_base(Parser_Module):
             restart = True
             while restart:
                 restart = False
-                for mod_id, mod in mods.get(base["domain"], {}).items():
-                    weight = next((weight["weight"] for weight in mod["spawn_weights"] if weight["tag"] in tags), 0)
-                    type = mod["generation_type"]
-                    if not weight:
-                        influence = next(
-                            (weight for weight in mod["spawn_weights"] if weight["tag"] in influence_tags), {}
+                for domain in [base["domain"], "delve"]:
+                    for mod_id, mod in mods_by_domain.get(domain, {}).items():
+                        delve = domain == "delve"
+
+                        weight = next(
+                            (weight["weight"] for weight in mod["spawn_weights"] if weight["tag"] in tags), None
                         )
-                        if influence:
-                            weight = influence["weight"]
-                            type = type + "_" + influence["tag"].split("_")[-1]
-                        else:
-                            continue
-                    mod_generation: dict = mods_data.setdefault(type, {})
-                    mod_group: dict = mod_generation.setdefault(mod["type"], {})
-                    mod_group[mod_id] = weight
-                    for added_tag in mod.get("adds_tags", []):
-                        if added_tag not in tags:
-                            restart = tags[added_tag] = True
-                            tags.move_to_end(added_tag, False)
-                    if restart:
-                        break
+                        gen_type = mod["generation_type"]
+                        if delve:
+                            gen_type = "delve_" + gen_type
+                        if not weight:
+                            influence = next(
+                                (weight for weight in mod["spawn_weights"] if weight["tag"] in influence_tags), {}
+                            )
+                            if influence:
+                                weight = influence["weight"]
+                                gen_type = gen_type + "_" + influence["tag"].split("_")[-1]
+                            else:
+                                continue
+                        mod_generation: dict = mods_data.setdefault(gen_type, {})
+                        mod_group: dict = mod_generation.setdefault(mod["type"], {})
+                        mod_group[mod_id] = weight
+                        for added_tag in mod.get("adds_tags", []):
+                            if added_tag not in tags:
+                                restart = tags[added_tag] = True
+                                tags.move_to_end(added_tag, False)
+                        if restart:
+                            break
+
+        for synth in requests.get(
+            "https://www.poewiki.net/index.php?title=Special:CargoExport&tables=synthesis_mods&format=json"
+            "&fields=synthesis_mods.item_class_ids__full%3Ditem_classes%2C+synthesis_mods.mod_ids__full%3Dmods"
+            "&group+by=synthesis_mods.mod_ids__full%2Csynthesis_mods.item_class_ids__full&order+by=&limit=2000"
+        ).json():
+            for item_class in synth["item_classes"]:
+                results: dict[str, dict] = root[item_classes[item_class]["name"]].setdefault("synthesis", {})
+                for mod_id in synth["mods"]:
+                    if mod_id == "SynthesisImplicitMaximumAttackDodge1":
+                        mod_id = "SynthesisImplicitSpellDamageSuppressed1_"
+                    mod = mods[mod_id]
+                    group = results.setdefault(mod["type"], [])
+                    if mod_id not in group:
+                        group.append(mod_id)
+
+        essences = self.relational_reader["Essences.dat64"]
+        keys = [k for k in essences.table_columns.keys() if k.endswith("ModsKey") and not k.startswith("Display")]
+        item_class_map = {k.replace(" ", "") + "_ModsKey": k for k in item_classes}
+        item_class_map["OneHandThrustingSword_ModsKey"] = "Thrusting One Hand Sword"
+        for essence in essences:
+            name = essence["BaseItemTypesKey"]["Name"]
+            if name == "Remnant of Corruption":
+                continue
+            level = name.split()[0]
+            type = name.split()[-1]
+            for key in keys:
+                result = root[item_classes[item_class_map[key]]["name"]].setdefault("essences", {})
+                result.setdefault(type, {})[level] = essence[key]["Id"]
 
         write_json(root, self.data_path, "mods_by_base")
 
